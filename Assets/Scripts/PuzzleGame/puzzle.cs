4bf3f1c11ee6ccd10f34ce67ac41a43e5f3ac88b
LukeArruda <lukeparruda@gmail.com>
2024-11-21 14:37:57 -0500

    puzzle.cs

--- Assets/Scripts/PuzzleGame/puzzle.cs
+++ Assets/Scripts/PuzzleGame/puzzle.cs
@@ -1,1 +1,21 @@
+using System;
 
+class Program
+{
+static void Main(string[] args)
+{
+//Grid positions for each piece (Placeholders for now, will be changed later)
+double pos1 = 1;
+double pos2 = 2;
+double pos3 = 3;
+double pos4 = 4;
+double pos5 = 5;
+double pos6 = 6;
+double pos7 = 7;
+double pos8 = 8;
+double pos9 = 9;
+//checks if all pieces are in the correct position
+bool completionCheck = //too lazy to do this rn sorry
+}
+}
+//I miss Lua :(
