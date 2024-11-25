c264cbb7442ff5f6290b33a9957f7ed9e488dc72
LukeArruda <lukeparruda@gmail.com>
2024-11-25 14:51:38 -0500

    Update puzzle.cs
    this gives me the Balkan rage

--- Assets/Scripts/PuzzleGame/puzzle.cs
+++ Assets/Scripts/PuzzleGame/puzzle.cs
@@ -14,8 +14,31 @@ double pos6 = 6;
 double pos7 = 7;
 double pos8 = 8;
 double pos9 = 9;
-//checks if all pieces are in the correct position
-bool completionCheck = //too lazy to do this rn sorry
+// Checks if all pieces are in the correct position (also placeholders)
+bool completionCheck = piece1pos == pos1 && piece2pos == pos2 && piece3pos == pos3 && piece4pos == pos4 && piece5pos == pos5 && pice6pos == pos6 && piece7pos == pos7 && piece8pos == pos8 && piece9pos == pos9;
+//timer(maybe)
+using System.Timers;
+int x = 30;
+// increased by 1 each second and prints value
+for (x > 0, --)
+{
+    Console.WriteLine(x);
+    wait(1000);
+    if (x == 0)
+    {
+        Console.WriteLine("TIME END");// prints when timer ends
+        Wait(2000);
+        if (completionCheck == true)        
+        {
+            Console.Write("GAME CLEAR!");//ifCorrect(Ashraful)
+        }
+        else
+        {
+            Console.WriteLine("GAME FAIL...");//ifFail(Ashraful)
+        }
+    }
+}
+//Can't confirm if this works or not
+//I still miss Lua :(
 }
 }
-//I miss Lua :(
